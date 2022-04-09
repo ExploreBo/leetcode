5473556bdd69c01c6e9b392bdc305b508632a31c
ExploreBo <haobo86@outlook.com>
2022-04-09 14:34:18 -0700

    Add O(h) memory solution of BST Iterator

--- Binary Tree/BinarySearchTreeIterator.java
+++ Binary Tree/BinarySearchTreeIterator.java
@@ -43,3 +43,50 @@ class BSTIterator {
         return q.size() != 0;
     }
 }
+
+
+// Solution 2: with O(h) memeory, h is the height of the tree
+// Use the stack as the data structure instead of doing a complete inorder traversal
+// the peak of the stack will always be the smallest number right now
+public class BSTIterator {
+    private final Stack<TreeNode> stack;
+    
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.left;
+        }
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode node = stack.pop();
+        
+        // Traversal cur node's right branch
+        TreeNode cur = node.right;
+        while (cur != null){
+            stack.push(cur);
+            cur = cur.left;
+        }
+        
+        return node.val;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
