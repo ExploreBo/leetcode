08f6f6f9f6bea25658739f67ed3ca55fa715cf58
ExploreBo <haobo86@outlook.com>
2022-04-10 13:18:08 -0700

    Add solution to post order traversal of BST

--- Binary Tree/BinaryTreePostorderTraversal.java
+++ Binary Tree/BinaryTreePostorderTraversal.java
@@ -37,21 +37,44 @@ class Solution {
         if (root == null) {
             return result;
         }
-        TreeNode cur = root;
+        // to be the last exit node
+        TreeNode pre = root;
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
         while (!stack.isEmpty()) {
             TreeNode peek = stack.peek();
-            if (peek.left != null && peek.left != cur && peek.right != cur) {
+            if (peek.left != null && peek.left != pre && peek.right != pre) {
+                // left and right have not been explored
                 stack.push(peek.left);
-            } else if (peek.right != null && peek.right != cur) {
+            } else if (peek.right != null && peek.right != pre) {
+                // left has been explored but not for right 
                 stack.push(peek.right);
             } else {
-                // leaf node
+                // leaf node, or left and right have been explored
                 result.add(stack.pop().val);
-                cur = peek;
+                pre = peek;
             }
         }
         return result;
     }
+}
+
+// Combine linkedlist and addFirst
+public List<Integer> postorderTraversal(TreeNode root) {
+    LinkedList<Integer> result = new LinkedList<>();
+    Stack<TreeNode> stack = new Stack<>();
+    if (root == null) return result;
+    
+    stack.push(root);
+    while (!stack.isEmpty()) {
+        TreeNode cur = stack.pop();
+        result.addFirst(cur.val);
+        if (cur.left != null) {
+            stack.push(cur.left);
+        }
+        if (cur.right != null) {
+            stack.push(cur.right);
+        } 
+    }
+    return result;
 }
