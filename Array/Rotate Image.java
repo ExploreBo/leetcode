874dd629958b73bd12a7a418f7a8b4036ec9492d
ExploreBo <haobo86@outlook.com>
2022-05-30 18:52:37 -0700

    Add two array problems

---
+++ Array/Rotate Image.java
@@ -0,0 +1,67 @@
+// in place.s
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - i - 1; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n-j-1] = matrix[j][n-i-1];
+                matrix[j][n - i - 1] = tmp;
+            }
+        }
+    }
+}
+
+// transpose and reflect
+class Solution {
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reflect(matrix);
+    }
+    
+    public void transpose(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+    }
+    
+    public void reflect(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+}
+
+// reflect rows and then columns
+class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i = 0; i< matrix.length; i++){
+            for(int j = i; j < matrix[0].length; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i =0 ; i < matrix.length; i++){
+            for(int j = 0; j < matrix.length / 2; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Completed Problems (Total: 173)
+# Completed Problems (Total: 175)
 
 ## Data Structure
 
@@ -45,6 +45,8 @@
 21. [Triangle Count](https://www.lintcode.com/problem/382)
 22. [Find All Duplicates in an Array](https://leetcode.com/problems/find-all-duplicates-in-an-array/)
 23. [Set Matrix Zeroes](https://leetcode.com/problems/set-matrix-zeroes/)
+24. [Rotate Image](https://leetcode.com/problems/rotate-image)
+25. [Spiral Matrix](https://leetcode.com/problems/spiral-matrix)
 
 ### Linked List
 1. [Add Two Numbers](https://leetcode.com/problems/add-two-numbers)
