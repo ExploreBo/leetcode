a87945cad54efcb382ec337eb9866ef4fc41a9dc
ExploreBo <haobo86@outlook.com>
2023-08-17 22:47:14 -0700

    Add a comment to SortColors

--- Sort/SortColors.java
+++ Sort/SortColors.java
@@ -28,7 +28,7 @@ class Solution {
 class Solution {
     public void sortColors(int[] nums) {
         int p1 = 0, p2 = nums.length - 1, index = 0;
-        while (index <= p2) {
+        while (index <= p2) { // index > p2 means we gonna check what has been swapped here as color2.
             if (nums[index] == 0) {
                 nums[index] = nums[p1];
                 nums[p1] = 0;
