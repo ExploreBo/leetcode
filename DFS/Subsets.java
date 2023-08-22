ba694c9336d30756cd5235ba7fba9400d016839a
ExploreBo <haobo86@outlook.com>
2023-08-21 23:16:00 -0700

    Small improvements on Subsets

--- DFS/Subsets.java
+++ DFS/Subsets.java
@@ -5,21 +5,19 @@ public class Solution {
         // [] is always a subset of any set
         result.add(solution);
         if (nums.length == 0) return result;
-        Arrays.sort(nums);
         helper(nums, solution, result, 0);
         return result;
     }
     
     private void helper (int[] nums, List<Integer> solution, List<List<Integer>> result, int start){
-        for (int i = start; i < nums.length; i++) {
+        for (int i = start; i < nums.length; ++i) {
             solution.add(nums[i]);
             // every step add the solution as a subset
             result.add(new ArrayList<Integer>(solution));
             if (start == nums.length) {
                 // no need to continue
                 return;
-            }
-            else {
+            } else {
                 // start from i + 1
                 helper(nums, solution, result, i + 1);
                 solution.remove(solution.size() - 1);
